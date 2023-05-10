0bf4dd1d234c12af8689819a366c612ac9a74a91
zohirul22 <shahin57islam@gmail.com>
2023-05-10 11:05:41 +0600

    slider section

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,9 +1,34 @@
 
-console.log('btn')
-let btnM = document.getElementById('btnM');
-let navLinks = document.querySelector('.nav-links');
 
-btnM.onclick = () =>{
-    btnM.classList.toggle('fa-times');
-    navLinks.classList.toggle('active')
-}
+// slider js 
+document.addEventListener("DOMContentLoaded", function() {
+    const sliderContainer = document.querySelector(".slider-container");
+    const prevBtn = document.querySelector(".prev-btn");
+    const nextBtn = document.querySelector(".next-btn");
+  
+    const slideWidth = sliderContainer.clientWidth;
+    let currentPosition = 0;
+  
+    prevBtn.addEventListener("click", function() {
+      currentPosition += slideWidth;
+      if (currentPosition > 0) {
+        currentPosition = -(sliderContainer.clientWidth - slideWidth);
+      }
+      sliderContainer.style.transform = `translateX(${currentPosition}px)`;
+    });
+  
+    nextBtn.addEventListener("click", function() {
+      currentPosition -= slideWidth;
+      if (currentPosition < -(sliderContainer.clientWidth - slideWidth)) {
+        currentPosition = 0;
+      }
+      sliderContainer.style.transform = `translateX(${currentPosition}px)`;
+    });
+  
+    window.addEventListener("resize", function() {
+      slideWidth = sliderContainer.clientWidth;
+      sliderContainer.style.transform = `translateX(0px)`;
+    });
+  });
+  
+  // slider js
